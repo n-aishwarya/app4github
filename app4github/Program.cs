f1cae66af1330e6485d21da21fa1ff2d90fd4025
n-aishwarya <naishwarya@gmail.com>
2022-08-11 10:07:51 +0530

    first commit

--- app4github/Program.cs
+++ app4github/Program.cs
@@ -7,6 +7,8 @@ namespace app4github
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello");
+
         }
     }
 }
